f34c32ffe183c03e0a43e99a69be1b377783a655
Anurag Dulapalli <pdulapalli@gmail.com>
2021-03-26 02:55:32 -0400

    Add Prisma module

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,9 +1,9 @@
-
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient
+export class PrismaService
+  extends PrismaClient
   implements OnModuleInit, OnModuleDestroy {
   async onModuleInit() {
     await this.$connect();
